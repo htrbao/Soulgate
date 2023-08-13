2a409b3d1af98aef45bb54ae69bbf3548687e208
Ha Thien Loc <ha.thienloc131@gmail.com>
2023-08-13 22:32:34 +0700

    checkcollision oke

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -7,12 +7,17 @@ public class Laser : MonoBehaviour
     public Camera cam;
     public LineRenderer lineRenderer;
     public Transform firePoint;
-
+    public GameObject startVFX;
+    public GameObject endVFX;
+    public GameObject character;
     public float targetTime = 3.0f;
     private Quaternion rotation;
+    private List<ParticleSystem> particles = new List<ParticleSystem>();
+    
     // Start is called before the first frame update
     void Start()
     {
+        FillLists();
         DisableLaser();
     }
 
@@ -45,21 +50,45 @@ public class Laser : MonoBehaviour
     {
         lineRenderer.enabled = true;
         targetTime = 3.0f;
+        for (int i = 0; i < particles.Count; i++)
+        {
+            particles[i].Play();
+        }
     }
 
     void UpdateLaser()
     {
         Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0f;
-        lineRenderer.SetPosition(0, firePoint.position - transform.position);
-        // Console.Log(firePoint)
 
-        lineRenderer.SetPosition(1, mousePos - transform.position);
+        lineRenderer.SetPosition(0, firePoint.position);
+
+        lineRenderer.SetPosition(1, mousePos);
+        Vector2 direction = mousePos - transform.position;
+        startVFX.transform.position = firePoint.position;
+        int layerMask = ~LayerMask.GetMask("Player");
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position,direction.normalized,direction.magnitude, layerMask);
+        for (int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit2D hit = hits[i];
+
+            if (hit && hit.collider.isTrigger == false)
+            {
+                lineRenderer.SetPosition(1, hit.point);
+            }
+        }
+
+        endVFX.transform.position = lineRenderer.GetPosition(1);
+
     }
 
     void DisableLaser()
     {
         lineRenderer.enabled = false;
+        for (int i = 0; i < particles.Count; i++)
+        {
+            particles[i].Stop();
+        }
     }
 
     // void RotateToMouse()
@@ -69,4 +98,26 @@ public class Laser : MonoBehaviour
     //     rotation.eulerAngles = new Vector3(0,0,angle);
     //     transform.rotation = rotation;
     // }
+
+    void FillLists()
+    {
+        for (int i = 0; i < startVFX.transform.childCount; i++)
+        {
+            var ps = startVFX.transform.GetChild(i).GetComponent<ParticleSystem>();
+            if (ps != null)
+            {
+                particles.Add(ps);
+            }
+        }
+
+        for (int i = 0; i < endVFX.transform.childCount; i++)
+        {
+            var ps = endVFX.transform.GetChild(i).GetComponent<ParticleSystem>();
+            if (ps != null)
+            {
+                particles.Add(ps);
+            }
+        }
+    }
+    
 }
